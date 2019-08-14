43fdc932e2400b066349fb5c089f05621350a95d
Andrew Chow <chowlick@gmail.com>
2019-08-13 17:12:07 -0700

    savepoint

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, HashRouter } from 'react-router-dom';
+import { withFirebase } from '../Firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      signedIn:false
+    }
+  }
+
+  onSubmit = () => {
+    this.props.firebase.googleLogin();
+    this.setState({ signedIn: true });
+    console.log('singed in');
+  }
+
+  logout = () => {
+    this.props.firebase.logout();
+  }
+
+
+
+
+  render() {
+    var auth = this.props.firebase.auth;
+    var user = auth.currentUser;
+    console.log(auth);
+    console.log(user);
+
+    return (
+      <div>
+        <button onClick={this.onSubmit}>login</button>
+        {
+          user ?
+            <button onClick={this.logout}>logout</button>
+            :
+            <div></div>
+        }
+      </div>
+
+    );
+  }
 }
 
-export default App;
+
+export default withFirebase(App);
+
+{/* <HashRouter>
+<div>
+  <div>
+    <Navigation forwardRef={node => this.node = node} 
+    newReviewsCount={this.state.newReviewsCount}
+     />
+    <hr />
+  </div>
+  <div className={classes.main} onClick={this.handleContainerClick}>
+    <Route exact path={ROUTES.TEST} component={TestPage} />
+    <Route
+      exact
+      path={ROUTES.PASSWORD_FORGET}
+      component={PasswordForgetPage}
+    />
+  </div>
+</div>
+</HashRouter> */}
--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import firebase from 'firebase';
 import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/storage';
@@ -16,59 +17,111 @@ class Firebase {
   constructor() {
     app.initializeApp(config);
 
-    this.auth = app.auth();
+    this.auth = firebase.auth();
+
+    // this.auth = app.auth();
     this.db = app.database();
     this.storage = app.storage();
   }
 
+  provider = new firebase.auth.GoogleAuthProvider();
+
+  googleLogin = () => {
+    firebase.auth().signInWithPopup(this.provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+      console.log(token, user);
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+  }
+
+  logout = () => {
+    firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+    }).catch(function(error) {
+      // An error happened.
+    });
+  }
+
+  // googleLoginPopup = firebase.auth().signInWithPopup(this.provider).then(function(result) {
+  //   // This gives you a Google Access Token. You can use it to access the Google API.
+  //   var token = result.credential.accessToken;
+  //   // The signed-in user info.
+  //   var user = result.user;
+  //   // ...
+  //   console.log(token, user);
+  //   firebase.auth().signInWithRedirect(this.provider);
+
+  // }).catch(function(error) {
+  //   // Handle Errors here.
+  //   var errorCode = error.code;
+  //   var errorMessage = error.message;
+  //   // The email of the user's account used.
+  //   var email = error.email;
+  //   // The firebase.auth.AuthCredential type that was used.
+  //   var credential = error.credential;
+  //   // ...
+  // });
+
   // *** Auth API ***
 
-  doCreateUserWithEmailAndPassword = (email, password) =>
-    this.auth.createUserWithEmailAndPassword(email, password);
+  // doCreateUserWithEmailAndPassword = (email, password) =>
+  //   this.auth.createUserWithEmailAndPassword(email, password);
 
-  doSignInWithEmailAndPassword = (email, password) =>
-    this.auth.signInWithEmailAndPassword(email, password);
+  // doSignInWithEmailAndPassword = (email, password) =>
+  //   this.auth.signInWithEmailAndPassword(email, password);
 
-  doSignOut = () => {
-    this.auth.signOut();
-  }
-    
+  // doSignOut = () => {
+  //   this.auth.signOut();
+  // }
 
-  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
 
-  doPasswordUpdate = password =>
-    this.auth.currentUser.updatePassword(password);
+  // doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  // doPasswordUpdate = password =>
+  //   this.auth.currentUser.updatePassword(password);
 
   // *** User API ***
   //Database
-  user = userid => this.db.ref(`users/${userid}`);
-  bobaShopUserReview = (shopName, userId) => this.db.ref(`bobaShopUserReviews/${shopName}/${userId}`);
-  bobaShopUserComment = (shopName, userId) => this.db.ref(`bobaShopUserReviews/${shopName}/${userId}/comments`);
-  userReview = (userId, shopName) => this.db.ref(`users/${userId}/reviews/${shopName}`);
-  userReviewComment = (userId, shopName) => this.db.ref(`users/${userId}/reviews/${shopName}/comments`);  
-  bobaShop = (bobaShop) => this.db.ref(`bobaShops/${bobaShop}`);
-  location = (location) => this.db.ref(`locations/${location}`);
-  userReviewLastVisit = (userid) => this.db.ref(`users/${userid}/reviewsLastVisit`);
+  // user = userid => this.db.ref(`users/${userid}`);
+  // bobaShopUserReview = (shopName, userId) => this.db.ref(`bobaShopUserReviews/${shopName}/${userId}`);
+  // bobaShopUserComment = (shopName, userId) => this.db.ref(`bobaShopUserReviews/${shopName}/${userId}/comments`);
+  // userReview = (userId, shopName) => this.db.ref(`users/${userId}/reviews/${shopName}`);
+  // userReviewComment = (userId, shopName) => this.db.ref(`users/${userId}/reviews/${shopName}/comments`);  
+  // bobaShop = (bobaShop) => this.db.ref(`bobaShops/${bobaShop}`);
+  // location = (location) => this.db.ref(`locations/${location}`);
+  // userReviewLastVisit = (userid) => this.db.ref(`users/${userid}/reviewsLastVisit`);
 
-  bobaShopReviews = () => this.db.ref(`bobaShopUserReviews`);
-  bobaShopUserReviews = (shopName) => this.db.ref(`bobaShopUserReviews/${shopName}`);
-  userReviews = (userId) => this.db.ref(`users/${userId}/reviews`);
-  users = () => this.db.ref('users');
-  bobaShops = () => this.db.ref('bobaShops');
-  locations = () => this.db.ref('locations');
-  reviewDateTimes = () => this.db.ref('reviewDateTimes');
+  // bobaShopReviews = () => this.db.ref(`bobaShopUserReviews`);
+  // bobaShopUserReviews = (shopName) => this.db.ref(`bobaShopUserReviews/${shopName}`);
+  // userReviews = (userId) => this.db.ref(`users/${userId}/reviews`);
+  // users = () => this.db.ref('users');
+  // bobaShops = () => this.db.ref('bobaShops');
+  // locations = () => this.db.ref('locations');
+  // reviewDateTimes = () => this.db.ref('reviewDateTimes');
 
-  bobaShopImages = (shopName) => this.db.ref(`bobaShops/${shopName}/images`);
-  bobaShopImage = (shopName, id) => this.db.ref(`bobaShops/${shopName}/images/${id}`);
-  userImages = (userId) => this.db.ref(`users/${userId}/images`);
-  userImage = (userId, id) => this.db.ref(`users/${userId}/images/${id}`);
-  images = () => this.db.ref(`images`);
-  image = (id) => this.db.ref(`images/${id}`);
+  // bobaShopImages = (shopName) => this.db.ref(`bobaShops/${shopName}/images`);
+  // bobaShopImage = (shopName, id) => this.db.ref(`bobaShops/${shopName}/images/${id}`);
+  // userImages = (userId) => this.db.ref(`users/${userId}/images`);
+  // userImage = (userId, id) => this.db.ref(`users/${userId}/images/${id}`);
+  // images = () => this.db.ref(`images`);
+  // image = (id) => this.db.ref(`images/${id}`);
 
 
-  //Storage
-  storageBobaShopImages = (bobaShop) => this.storage.ref(`bobaShops/${bobaShop}/images`);
-  storageBobaShopImage = (bobaShop, imageName) => this.storage.ref(`bobaShops/${bobaShop}/images/${imageName}`);
+  // //Storage
+  // storageBobaShopImages = (bobaShop) => this.storage.ref(`bobaShops/${bobaShop}/images`);
+  // storageBobaShopImage = (bobaShop, imageName) => this.storage.ref(`bobaShops/${bobaShop}/images/${imageName}`);
 
 
 }
