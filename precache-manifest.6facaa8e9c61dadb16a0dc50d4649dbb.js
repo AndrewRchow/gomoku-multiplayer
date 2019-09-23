c9032f7707e4b1af2694bd649fd7222be2a0490c
Andrew Chow <chowlick@gmail.com>
2019-09-23 12:12:26 -0700

    Updates

---
+++ precache-manifest.6facaa8e9c61dadb16a0dc50d4649dbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a60fe29e1bba2502244933eac0850ee2",
+    "url": "/gomoku-multiplayer/index.html"
+  },
+  {
+    "revision": "1ed4574ac09bea63f046",
+    "url": "/gomoku-multiplayer/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "7fbd6136c84d11697320",
+    "url": "/gomoku-multiplayer/static/css/main.9551ad0a.chunk.css"
+  },
+  {
+    "revision": "1ed4574ac09bea63f046",
+    "url": "/gomoku-multiplayer/static/js/2.356cc6ad.chunk.js"
+  },
+  {
+    "revision": "7fbd6136c84d11697320",
+    "url": "/gomoku-multiplayer/static/js/main.c09020a2.chunk.js"
+  },
+  {
+    "revision": "81cc733fa564f19ca712",
+    "url": "/gomoku-multiplayer/static/js/runtime~main.f550bee3.js"
+  }
+]);
